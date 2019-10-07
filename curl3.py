36a39d3b55dc34b40a85ca1a030adc7c396c3507
Hulto <jack.m.mckenna@gmail.com>
2019-10-07 08:05:42 -0400

    debug

--- curl3.py
+++ curl3.py
@@ -258,6 +258,7 @@ class Crawler():
     """
     def dump_queue(self, queue: Queue) -> List:
         res = []
+        print("Dumbing the queue")
         # While the queue is not empty copy items to List
         while(not queue.empty()):
             res.append(self.work_queue.get())
